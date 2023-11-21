3cf95d70db28cd7405efd4c182ddee53e0e5dde6
Leon Work <26638409+Ddraigan@users.noreply.github.com>
2023-11-21 14:30:42 +0000

    trimmed lines

--- 2022/days/day-two/src/main.rs
+++ 2022/days/day-two/src/main.rs
@@ -1,7 +1,12 @@
 use std::fs::read_to_string;
 
 fn main() {
-    let contents = read_to_string("src/day_one").expect("Unable to read file");
-    let lines: Vec<&str> = contents.split('\n').collect();
-    println!("{:?}", lines)
+    let contents = read_to_string("src/day_two.txt").expect("Unable to read file");
+    let lines: Vec<&str> = contents.split('\r').collect();
+    println!("{:?}", lines);
+    let mut trimmed_lines = vec![];
+    for line in lines {
+        trimmed_lines.push(line.trim())
+    }
+    println!("{:?}", trimmed_lines)
 }
