a3c45b45f3f03986417765b0796b0de8c5482c89
Leon Work <26638409+Ddraigan@users.noreply.github.com>
2023-11-21 15:23:29 +0000

    impl tryfrom for rps

--- 2022/days/day-two/src/main.rs
+++ 2022/days/day-two/src/main.rs
@@ -2,28 +2,38 @@ use std::fs::read_to_string;
 
 fn main() {
     let contents = read_to_string("src/day_two.txt").expect("Unable to read file");
-    let lines: Vec<&str> = contents.split('\r').collect();
-    println!("{:?}", lines);
-    let mut trimmed_lines = vec![];
-    for line in lines {
-        trimmed_lines.push(line.trim())
-    }
-    println!("{:?}", trimmed_lines)
+    let rps_strategy = convert_to_rps(contents);
+    println!("{:?}", rps_strategy)
 }
 
+fn convert_to_rps(lines: String) -> Vec<(RPS, RPS)> {
+    let lines: Vec<&str> = lines.split('\r').collect();
+    let mut converted_lines = vec![];
+    for line in lines {
+        let trimmed_line = line.trim();
+        let mut split = trimmed_line.split_whitespace();
+        let a = RPS::try_from(split.next().unwrap().chars().nth(0).unwrap()).unwrap();
+        let b = RPS::try_from(split.next().unwrap().chars().nth(0).unwrap()).unwrap();
+        converted_lines.push((a, b))
+    }
+    converted_lines
+}
+
+#[derive(Debug)]
 enum RPS {
     Rock = 1,
     Paper = 2,
     Scissors = 3,
 }
 
-impl RPS {
-    fn into_rps(char: char) -> Option<RPS> {
-        match char {
-            'A' | 'X' => Some(RPS::Rock),
-            'B' | 'Y' => Some(RPS::Paper),
-            'C' | 'Z' => Some(RPS::Scissors),
-            _ => None,
+impl TryFrom<char> for RPS {
+    type Error = ();
+    fn try_from(value: char) -> Result<Self, Self::Error> {
+        match value {
+            'A' | 'X' => Ok(RPS::Rock),
+            'B' | 'Y' => Ok(RPS::Paper),
+            'C' | 'Z' => Ok(RPS::Scissors),
+            _ => Err(()),
         }
     }
 }
