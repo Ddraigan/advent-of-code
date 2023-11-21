fe0ccebd820687f4c27b460cda86e2d06a93711d
Leon Work <26638409+Ddraigan@users.noreply.github.com>
2023-11-21 17:22:17 +0000

    Math fun

--- 2022/days/day-two/src/main.rs
+++ 2022/days/day-two/src/main.rs
@@ -3,15 +3,15 @@ use std::fs::read_to_string;
 fn main() {
     let contents = read_to_string("src/day_two.txt").expect("Unable to read file");
     let rps_strategy = convert_to_rps(contents);
-    println!("{:?}", rps_strategy)
+    let points = play_match(rps_strategy);
+    println!("{}", points)
 }
 
 fn convert_to_rps(lines: String) -> Vec<(RPS, RPS)> {
-    let lines: Vec<&str> = lines.split('\r').collect();
+    let lines: Vec<&str> = lines.split("\r\n").collect();
     let mut converted_lines = vec![];
     for line in lines {
-        let trimmed_line = line.trim();
-        let mut split = trimmed_line.split_whitespace();
+        let mut split = line.split_whitespace();
         let a = RPS::try_from(split.next().unwrap().chars().nth(0).unwrap()).unwrap();
         let b = RPS::try_from(split.next().unwrap().chars().nth(0).unwrap()).unwrap();
         converted_lines.push((a, b))
@@ -19,7 +19,40 @@ fn convert_to_rps(lines: String) -> Vec<(RPS, RPS)> {
     converted_lines
 }
 
-#[derive(Debug)]
+fn play_match(rps_strategy: Vec<(RPS, RPS)>) -> usize {
+    let mut points: usize = 0;
+
+    for game in rps_strategy {
+        points += get_winner(&game.0, &game.1);
+        let x = game.1 as usize;
+        println!("{x}");
+        points += x
+    }
+
+    points
+}
+
+fn get_winner(opponent: &RPS, player: &RPS) -> usize {
+    let x = if opponent < player {
+        if player == &RPS::Rock {
+            0
+        } else {
+            6
+        }
+    } else if opponent > player {
+        if player == &RPS::Scissors {
+            6
+        } else {
+            0
+        }
+    } else {
+        3
+    };
+    println!("{:#?}", x);
+    x
+}
+
+#[derive(Debug, PartialEq, PartialOrd)]
 enum RPS {
     Rock = 1,
     Paper = 2,
@@ -37,3 +70,5 @@ impl TryFrom<char> for RPS {
         }
     }
 }
+
+// "6 3 3 6 0 3 3 6 0 3 3 0"
