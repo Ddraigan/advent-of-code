30bc15cd2cc1c2384e75fa89915a838344cb7e0f
Leon Work <26638409+Ddraigan@users.noreply.github.com>
2023-12-01 16:51:16 +0000

    part two

--- 2023/days/one/src/main.rs
+++ 2023/days/one/src/main.rs
@@ -4,9 +4,29 @@ use std::{
     path::Path,
 };
 
+const NUMBERWORDS: [(&'static str, usize); 9] = [
+    ("one", 1),
+    ("two", 2),
+    ("three", 3),
+    ("four", 4),
+    ("five", 5),
+    ("six", 6),
+    ("seven", 7),
+    ("eight", 8),
+    ("nine", 9),
+];
+
 fn main() {
+    // Part One
     let lines = lines_from_file("src/input.txt").expect("File to exist");
-    let numbers = parse_numbers(lines);
+    let numbers = part_one_parse_numbers(lines);
+    let number = get_number(numbers);
+
+    println!("{:?}", number);
+
+    // Part Two
+    let lines = lines_from_file("src/input.txt").expect("File to exist");
+    let numbers = part_two_parse_numbers(&lines);
     let number = get_number(numbers);
 
     println!("{:?}", number);
@@ -16,7 +36,42 @@ fn get_number(numbers: Vec<usize>) -> usize {
     numbers.iter().sum()
 }
 
-fn parse_numbers(lines: Vec<String>) -> Vec<usize> {
+fn part_two_parse_numbers(lines: &Vec<String>) -> Vec<usize> {
+    let mut numbers = vec![];
+
+    for line in lines {
+        let mut nums_in_line: Vec<(usize, usize)> = vec![];
+        let i = line.find(|c: char| c.is_ascii_digit()).unwrap();
+        let v = &line.chars().nth(i).unwrap();
+        nums_in_line.push((i, *v as usize));
+
+        for (word, number) in NUMBERWORDS {
+            match line.find(word) {
+                Some(i) => nums_in_line.push((i, number)),
+                None => {}
+            }
+        }
+
+        let i = line.rfind(|c: char| c.is_ascii_digit()).unwrap();
+        let v = &line.chars().nth(i).unwrap();
+        nums_in_line.push((i, *v as usize));
+
+        nums_in_line.sort_by_key(|k| k.0);
+
+        let first_num = nums_in_line[0].0.to_string();
+        let last_num = nums_in_line.last().unwrap().0.to_string();
+
+        let number = format!("{}{}", first_num, last_num)
+            .parse()
+            .expect("Valid Number");
+
+        numbers.push(number);
+    }
+
+    numbers
+}
+
+fn part_one_parse_numbers(lines: Vec<String>) -> Vec<usize> {
     let mut numbers = vec![];
 
     for line in lines {
