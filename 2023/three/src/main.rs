e950129b464adfe94c9f8f96fd113f0eaf46573f
Leon Work <26638409+Ddraigan@users.noreply.github.com>
2023-12-05 17:01:24 +0000

    basic

--- 2023/three/src/main.rs
+++ 2023/three/src/main.rs
@@ -1,3 +1,30 @@
+use std::{fs, path::Path};
+
 fn main() {
-    println!("Hello, world!");
+    println!("{:?}", lines_from_file("src/input.txt"))
+}
+
+#[derive(Debug)]
+struct Point {
+    x: u8,
+    y: u8,
+}
+
+#[derive(Debug)]
+struct Grid {
+    content: Vec<Vec<char>>,
+}
+
+impl Grid {
+    // fn new(content: String) -> Self {
+    //     let content = Self::parse_content(content);
+    //
+    //     Self { content }
+    // }
+
+    fn parse_content(content: String) {}
+}
+
+fn lines_from_file(path: impl AsRef<Path>) -> String {
+    fs::read_to_string(path).expect("File to be there")
 }
