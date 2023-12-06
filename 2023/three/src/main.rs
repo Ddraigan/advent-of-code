1e0e7eca4195a5d39898e6598a6280594730adbc
Leon Work <26638409+Ddraigan@users.noreply.github.com>
2023-12-06 14:43:21 +0000

    progress

--- 2023/three/src/main.rs
+++ 2023/three/src/main.rs
@@ -1,22 +1,60 @@
-use std::{fs, path::Path};
+use std::{fs, path::Path, vec};
 
 fn main() {
     let lines = file_to_string("src/input.txt");
     let grid = Grid::new(&lines);
 
-    println!("{:#?}", grid.find_symbols());
+    // println!("{:#?}", grid.find_symbols());
+    println!("{:#?}", grid.part_one());
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 struct Point {
-    x: u8,
-    y: u8,
-    value: char,
+    x: i32,
+    y: i32,
 }
 
 impl Point {
-    fn new(x: u8, y: u8, value: char) -> Self {
-        Self { x, y, value }
+    fn new(x: i32, y: i32) -> Self {
+        Self { x, y }
+    }
+
+    fn neighbours(&self) -> [Point; 8] {
+        let neighbours: [Point; 8] = [
+            Point {
+                x: self.x + 1,
+                y: self.y + 1,
+            },
+            Point {
+                x: self.x + 1,
+                y: self.y,
+            },
+            Point {
+                x: self.x + 1,
+                y: self.y - 1,
+            },
+            Point {
+                x: self.x,
+                y: self.y + 1,
+            },
+            Point {
+                x: self.x,
+                y: self.y - 1,
+            },
+            Point {
+                x: self.x - 1,
+                y: self.y - 1,
+            },
+            Point {
+                x: self.x - 1,
+                y: self.y,
+            },
+            Point {
+                x: self.x - 1,
+                y: self.y + 1,
+            },
+        ];
+        neighbours
     }
 }
 
@@ -32,41 +70,57 @@ impl Grid {
         Self { content }
     }
 
-    fn part_one(&self) {
-        let neighbours: [(i32, i32); 8] = [
-            (1, 1),
-            (1, 0),
-            (1, -1),
-            (0, 1),
-            (0, -1),
-            (-1, -1),
-            (-1, 0),
-            (-1, 1),
-        ];
+    fn part_one(&self) -> usize {
+        let mut numbers: Vec<usize> = vec![];
+
+        for (y, line) in self.content.iter().enumerate() {
+            for (x, char) in line.iter().enumerate() {
+                if !char.is_symbol() {
+                    continue;
+                }
+
+                let point = Point::new(
+                    x.try_into().expect("Within bounds of u8"),
+                    y.try_into().expect("Within bounds of u8"),
+                );
+
+                for neighbour in point.neighbours() {
+                    if !self.point(&neighbour).is_ascii_digit() {
+                        continue;
+                    }
+                    println!("{}", self.point(&neighbour))
+                }
+            }
+        }
+
+        numbers.into_iter().sum()
     }
 
-    fn find_symbols(&self) -> Vec<Point> {
-        self.content
-            .iter()
-            .enumerate()
-            .flat_map(|(y, line)| {
-                line.iter().enumerate().filter_map(move |(x, char)| {
-                    if (*char as u8) == 61
-                        || (*char as u8) == 64
-                        || (*char as u8) < 48 && (*char as u8) != 46
-                    {
-                        Some(Point::new(
-                            x.try_into().expect("Number to be within bounds of u8"),
-                            y.try_into().expect("Number to be within bounds of u8"),
-                            *char,
-                        ))
-                    } else {
-                        None
-                    }
-                })
-            })
-            .collect()
+    fn point(&self, point: &Point) -> &char {
+        &self.content[point.y as usize][point.x as usize]
     }
+
+    // fn find_symbols(&self) -> Vec<Point> {
+    //     self.content
+    //         .iter()
+    //         .enumerate()
+    //         .flat_map(|(y, line)| {
+    //             line.iter().enumerate().filter_map(move |(x, char)| {
+    //                 if (*char as u8) == 61
+    //                     || (*char as u8) == 64
+    //                     || (*char as u8) < 48 && (*char as u8) != 46
+    //                 {
+    //                     Some(Point::new(
+    //                         x.try_into().expect("Number to be within bounds of u8"),
+    //                         y.try_into().expect("Number to be within bounds of u8"),
+    //                     ))
+    //                 } else {
+    //                     None
+    //                 }
+    //             })
+    //         })
+    //         .collect()
+    // }
 
     fn parse_content(content: &str) -> Vec<Vec<char>> {
         content
@@ -77,6 +131,41 @@ impl Grid {
     }
 }
 
+trait Symbol {
+    fn is_symbol(&self) -> bool;
+}
+
+impl Symbol for char {
+    fn is_symbol(&self) -> bool {
+        if (*self as u8) == 61 || (*self as u8) == 64 || (*self as u8) < 48 && (*self as u8) != 46 {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
 fn file_to_string(path: impl AsRef<Path>) -> String {
     fs::read_to_string(path).expect("File to be exist")
 }
+
+// #[test]
+// fn part_one_test() {
+//     let control = [
+//         Point { x: 121, y: 139 },
+//         Point { x: 121, y: 138 },
+//         Point { x: 121, y: 137 },
+//         Point { x: 120, y: 139 },
+//         Point { x: 120, y: 137 },
+//         Point { x: 119, y: 137 },
+//         Point { x: 119, y: 138 },
+//         Point { x: 119, y: 139 },
+//     ];
+//
+//     let lines = file_to_string("src/input.txt");
+//     let grid = Grid::new(&lines);
+//
+//     println!("{:#?}", grid.part_one().last());
+//
+//     assert!(&control == grid.part_one().last().unwrap())
+// }
