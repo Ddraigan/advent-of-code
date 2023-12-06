4a5f6c398fabd5f46c071a51854974381231d710
Leon Work <26638409+Ddraigan@users.noreply.github.com>
2023-12-06 17:16:39 +0000

    M

--- 2023/four/src/main.rs
+++ 2023/four/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let _lines = lines_from_file("src/input.txt");
+}
+
+fn lines_from_file(path: impl AsRef<std::path::Path>) -> std::io::Result<Vec<String>> {
+    let file = std::fs::File::open(path)?;
+    let buf = std::io::BufReader::new(file);
+    std::io::BufRead::lines(buf).collect()
 }
