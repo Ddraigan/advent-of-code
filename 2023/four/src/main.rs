aa6c201d587d6ae7f2ae7a1e8379fd133a145f7a
Leon Work <26638409+Ddraigan@users.noreply.github.com>
2023-12-08 13:19:26 +0000

    parsing

--- 2023/four/src/main.rs
+++ 2023/four/src/main.rs
@@ -1,8 +1,51 @@
 fn main() {
-    let _lines = lines_from_file("src/input.txt");
+    let lines = lines_from_file("src/input.txt").unwrap();
+
+    let cards: Vec<Card> = lines.iter().map(|line| Card::from(line.as_str())).collect();
+
+    println!("{cards:?}")
 }
 
-fn lines_from_file(path: impl AsRef<std::path::Path>) -> std::io::Result<Vec<String>> {
+#[derive(Debug)]
+struct Card {
+    id: u8,
+    winning_numbers: Vec<u32>,
+    card_numbers: Vec<u32>,
+}
+
+impl Card {}
+
+impl From<&str> for Card {
+    fn from(value: &str) -> Self {
+        let value = value.split(&[':', '|']).collect::<Vec<_>>();
+
+        let id = value[0].split_whitespace().collect::<Vec<&str>>()[1]
+            .parse()
+            .unwrap();
+
+        let winning_numbers = value[1]
+            .split_whitespace()
+            .collect::<Vec<&str>>()
+            .iter()
+            .map(|item| item.parse().unwrap())
+            .collect();
+
+        let card_numbers = value[2]
+            .split_whitespace()
+            .collect::<Vec<&str>>()
+            .iter()
+            .map(|item| item.parse().unwrap())
+            .collect();
+
+        Self {
+            id,
+            winning_numbers,
+            card_numbers,
+        }
+    }
+}
+
+fn lines_from_file(path: &str) -> std::io::Result<Vec<String>> {
     let file = std::fs::File::open(path)?;
     let buf = std::io::BufReader::new(file);
     std::io::BufRead::lines(buf).collect()
